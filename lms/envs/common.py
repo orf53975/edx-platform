8332be6a4c9970c57fd55a089b64b92f7ca8728d
syedimranhassan <imran@HammadHussain-Laptop-128.arbisoft.com>
2019-01-18 15:28:00 +0500

    test celery configuration change

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -962,7 +962,7 @@ MEDIA_ROOT = '/edx/var/edxapp/media/'
 MEDIA_URL = '/media/'
 
 # Locale/Internationalization
-TIME_ZONE = 'America/New_York'  # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+TIME_ZONE = 'Europe/London'  # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 LANGUAGE_CODE = 'en'  # http://www.i18nguy.com/unicode/language-identifiers.html
 # these languages display right to left
 LANGUAGES_BIDI = ("he", "ar", "fa", "ur", "fa-ir", "rtl")
